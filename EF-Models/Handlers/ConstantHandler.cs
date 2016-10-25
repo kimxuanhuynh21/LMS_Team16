804fe856533af27aadd87841f7df852ff7b5f18d
Duy <mr.huynhhuuduy@gmail.com>
2016-10-25 19:46:36 +0700

    demo tinh trang

---
+++ EF-Models/Handlers/ConstantHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel;
+
+namespace EF_Models.Handlers
+{
+    class ConstantHandler
+    {
+        public enum TinhTrang_DauSach
+        {
+            [Description("Cho Muon Ve Nha")]
+            MuonVeNha = 0,
+            [Description("Muon Doc Tai Cho")]
+            DocTaiCho = 1,
+            [Description("Dang Ki Photo")]
+            DangKiPhoto = 2,
+        }
+    }
+}
