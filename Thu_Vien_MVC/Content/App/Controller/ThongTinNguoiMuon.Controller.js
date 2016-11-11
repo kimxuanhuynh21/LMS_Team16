9587aad9c95b1fdff42006660df4994961bf86bd
xuan <kimxuanhuynh21@gmail.com>
2016-11-11 21:52:49 +0700

    controller ThongTinNguoiMuon.Controller.js

---
+++ Thu_Vien_MVC/Content/App/Controller/ThongTinNguoiMuon.Controller.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('LMSApp')
+        .controller('ThongTinNguoiMuonCtrl', ThongTinNguoiMuonCtrl);
+
+    ThongTinNguoiMuonCtrl.$inject = ['$scope'];
+
+    function ThongTinNguoiMuonCtrl($scope) {
+        $scope.test = "Xuân";
+
+        
+    }
+})();
